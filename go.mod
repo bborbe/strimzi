e313e3809fa8ee7829992e3905107ebb6b879ddb
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-02-15 08:19:28 +0100

    add topic deployer

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/bborbe/strimzi
 go 1.21.7
 
 require (
+	github.com/bborbe/errors v1.2.0
+	github.com/golang/glog v1.2.0
 	github.com/google/addlicense v1.1.1
 	github.com/incu6us/goimports-reviser v0.1.6
 	github.com/kisielk/errcheck v1.6.3
 	github.com/maxbrunsfeld/counterfeiter/v6 v6.7.0
-	github.com/onsi/ginkgo/v2 v2.13.1
+	github.com/onsi/ginkgo/v2 v2.13.2
 	github.com/onsi/gomega v1.30.0
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616
 	golang.org/x/vuln v1.0.1
@@ -16,7 +18,7 @@ require (
 )
 
 require (
-	github.com/bmatcuk/doublestar/v4 v4.0.2 // indirect
+	github.com/bmatcuk/doublestar/v4 v4.6.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
@@ -30,7 +32,7 @@ require (
 	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
-	github.com/google/pprof v0.0.0-20210720184732-4bb14d4b1be1 // indirect
+	github.com/google/pprof v0.0.0-20231205033806-a5a03c77bf08 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -39,17 +41,17 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/mod v0.13.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/oauth2 v0.8.0 // indirect
-	golang.org/x/sync v0.4.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
-	golang.org/x/term v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/term v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	golang.org/x/tools v0.14.0 // indirect
+	golang.org/x/tools v0.16.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ strimzi_topic-deployer.go
@@ -0,0 +1,72 @@
+// Copyright (c) 2024 Benjamin Borbe All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package strimzi
+
+import (
+	"context"
+
+	"github.com/bborbe/errors"
+	"github.com/golang/glog"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/bborbe/strimzi/k8s/apis/kafka.strimzi.io/v1beta2"
+	"github.com/bborbe/strimzi/k8s/client/clientset/versioned"
+)
+
+//counterfeiter:generate -o mocks/topic-deployer.go --fake-name TopicDeployer . TopicDeployer
+type TopicDeployer interface {
+	Deploy(ctx context.Context, topic v1beta2.KafkaTopic) error
+	Undeploy(ctx context.Context, namespace string, name string) error
+}
+
+func NewTopicDeployer(
+	clientset *versioned.Clientset,
+) TopicDeployer {
+	return &topicDeployer{
+		clientset: clientset,
+	}
+}
+
+type topicDeployer struct {
+	clientset *versioned.Clientset
+}
+
+func (t *topicDeployer) Deploy(ctx context.Context, topic v1beta2.KafkaTopic) error {
+	currentTopic, err := t.clientset.KafkaV1beta2().KafkaTopics(topic.Namespace).Get(ctx, topic.Name, metav1.GetOptions{})
+	if err != nil {
+		_, err = t.clientset.KafkaV1beta2().KafkaTopics(topic.Namespace).Create(ctx, &topic, metav1.CreateOptions{})
+		if err != nil {
+			return errors.Wrap(ctx, err, "create topic failed")
+		}
+		glog.V(3).Infof("topic %s created successful", topic.Name)
+		return nil
+	}
+	updateTopic := mergeTopic(*currentTopic, topic)
+	_, err = t.clientset.KafkaV1beta2().KafkaTopics(topic.Namespace).Update(ctx, &updateTopic, metav1.UpdateOptions{})
+	if err != nil {
+		return errors.Wrap(ctx, err, "update topic failed")
+	}
+	glog.V(3).Infof("topic %s updated successful", topic.Name)
+	return nil
+}
+
+func (t *topicDeployer) Undeploy(ctx context.Context, namespace string, name string) error {
+
+	_, err := t.clientset.KafkaV1beta2().KafkaTopics(namespace).Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		glog.V(3).Infof("topic '%s' not found => skip", name)
+		return nil
+	}
+	if err := t.clientset.KafkaV1beta2().KafkaTopics(namespace).Delete(ctx, name, metav1.DeleteOptions{}); err != nil {
+		return err
+	}
+	glog.V(3).Infof("delete %s completed", name)
+	return nil
+}
+
+func mergeTopic(current, new v1beta2.KafkaTopic) v1beta2.KafkaTopic {
+	new.ObjectMeta.ResourceVersion = current.ObjectMeta.ResourceVersion
+	return new
+}
